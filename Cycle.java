15dfba23f6167c88c3e4851f63ad1890457795b1
gsdeepika <gsdeepika.xworkz@gmail.com>
2023-06-27 16:02:46 +0530

    this is my first commit

---
+++ Cycle.java
@@ -0,0 +1,11 @@
+class Cycle{
+public static void main(String[] args)
+{
+System.out.println("i like my cycle");
+System.out.println("my cycle colour is black");
+System.out.println("i have a gear cycle");
+System.out.println("my brother has buy two cycle");
+System.out.println("in my childhood i use cycle daily");
+}
+}
+
