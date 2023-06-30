ad33f5339db3689f17c28106652ecea8f9ec8fe8
gsdeepika <gsdeepika.xworkz@gmail.com>
2023-06-30 13:50:05 +0530

    trying folder

---
+++ test/test.java
@@ -0,0 +1,63 @@
+a) 29
+c) 20
+c) 96
+c) 63
+a) 9
+a) k
+a) p
+b) FR
+c) bccab
+a) GOV
+a) 2
+c) 38
+d) window
+a) sun
+b) BE
+
+                                 JAVA
+								 
+1.  java is a object oriented programming language.
+    javac is a java compailer used to compile the program.
+	
+2.  JDK : java development kit, it is used to write the program
+    JRE : java runtime environment, it is used to execute the program
+    JVM : java virtual machine, it is used to execute the actual program  
+	
+4.  class - class is program or nonprimitive or blue print 
+
+5.  Array is a container which is used to store and retrive the reference value.
+
+9.  java is a object oriented programming language
+
+10. variable is a container used to store the data values.
+     types:
+	 *primitive 
+	 *non primitive
+	 
+11. variable defined in the class whose object is same copy is known as class variable.
+
+12. datatype specify the different size and values can be stored in variable.
+    *Primitive datatype
+     byte      float         char    boolean
+	 short     double
+	 int
+	 long
+    *NonPrimitive datatype
+	
+13. *java is a object oriented programming language
+    *java class is run in jdk
+	*java is a platform independent
+	
+14. *single dimentional array
+    *multi dimentional array
+	
+16. system.out.println() is a statement, it is used to print the message.
+
+17. it is used to point the same memory location address of reference variable in constant pool.
+
+18. dot operator is used to access the information
+
+19. object class
+
+20. java keywords:
+    variable,method,class,byte,char,default,if,for,boolean,float.
