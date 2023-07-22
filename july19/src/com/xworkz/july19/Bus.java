1f4cd70a875f0315e3f4a7adf3468d51ba8a6caf
gsdeepika <gsdeepika.xworkz@gmail.com>
2023-07-22 13:29:02 +0530

    test

---
+++ july19/src/com/xworkz/july19/Bus.java
@@ -0,0 +1,13 @@
+package com.xworkz.july19;
+
+public class Bus{
+Bus()
+{
+	System.out.println("Bus is slow");
+}
+
+public static void main(String[] args) {
+	System.out.println("running main");
+    Bus bus=new Bus();
+}
+}
---
+++ july19/src/com/xworkz/july19/BusRunner.java
@@ -0,0 +1,10 @@
+package com.xworkz.july19;
+
+public class BusRunner {
+
+	public static void main(String[] args) {
+		System.out.println("running main");
+	    Bus.slow();
+	}
+
+}
